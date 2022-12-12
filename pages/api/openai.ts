3f51f8000fe45df428d3933c4d232d0a980c69b1
yuikoito <toffywing@gmail.com>
2022-12-13 00:38:50 +0900

    chore: connect open ai

---
+++ pages/api/openai.ts
@@ -0,0 +1,20 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: `以下のコードをレビューしてください。もし問題点がある場合は教えて下さい。また、もっと良い書き方がある場合は教えて下さい。${req.body.code}`,
+    temperature: 0.5,
+    max_tokens: 2048,
+  });
+  res.status(200).json({ result: completion.data.choices[0].text });
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,22 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Textarea, Text, Button, Container } from "@chakra-ui/react";
+import axios from "axios";
 import Head from "next/head";
-import React from "react";
+import React, { useState } from "react";
 
 export default function Home() {
+  const [value, setValue] = useState<string>();
+  const [result, setResult] = useState<string>();
+  const handleInputChange = (e: {
+    target: { value: React.SetStateAction<string | undefined> };
+  }) => {
+    setValue(e.target.value);
+  };
+  const handleCodeCheck = async () => {
+    const res = await axios.post("/api/openai", {
+      code: value,
+    });
+    setResult(res.data.result);
+  };
   return (
     <>
       <Head>
@@ -10,7 +24,18 @@ export default function Home() {
         <meta name="description" content="Automatic code review by AI" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Box></Box>
+      <Container>
+        <Text mb="8px">コードレビュー</Text>
+        <Textarea
+          value={value}
+          onChange={handleInputChange}
+          placeholder="Here is a sample placeholder"
+          size="sm"
+        />
+        <Button onClick={handleCodeCheck}>コードレビューをする！</Button>
+        <Text>結果</Text>
+        <Box>{result}</Box>
+      </Container>
     </>
   );
 }
